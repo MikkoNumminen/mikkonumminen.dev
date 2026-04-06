c916c1cd9f09294b50d362e21a187530b9d46116
mikko numminen <mikko@mikkos-MacBook-Pro.local>
2026-04-06 17:43:00 +0300

    chore: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mikkonumminen.dev',
   output: 'static',
+  integrations: [sitemap()],
   build: {
     inlineStylesheets: 'auto',
   },
